2d6a933f4f3f12b6c3e31a3effb81b59c2ab57f9
samlai <sub20cuber@gmail.com>
2018-10-25 20:13:30 +0100

    test

--- src/cubedb/Db.java
+++ src/cubedb/Db.java
@@ -1,9 +1,8 @@
-package cubedb;
+package cubedb; //cubedb
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 
 public class Db {
 
